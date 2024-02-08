0c81e48bb0b9ae2dfe8e3a5d3c4e9723cf8414cb
Krzysztof Lachota <krzysztof.lachota@gmail.com>
2024-02-08 20:53:26 +0100

    Recovered after failure

--- src/main/java/pl/isa/javaers/Alerts.java
+++ src/main/java/pl/isa/javaers/Alerts.java
@@ -1,96 +1,53 @@
 package pl.isa.javaers;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static pl.isa.javaers.ErrorCodes.ASSETS_WRONGCCODE;
+import static pl.isa.javaers.Settings.ALERTS_FILE;
 
 public class Alerts {
-        public static List<Alert> alerts = new ArrayList<>();
+    public static List<Alert> alerts = new ArrayList<>();
 
 
-        public void loadAlerts() {
-            try {
-                byte[] data = Files.readAllBytes(Paths.get("src/main/resources/alerts.json"));
-                System.out.println(Arrays.toString(data));
-                ObjectMapper objectMapper = new ObjectMapper();
-                //objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); - gubi wszystkie dane
-                //ArrayList<Alert> alerts = objectMapper.readValue(data, new TypeReference<ArrayList<Alert>>() {}); - nie działa
-                //Alert alert = objectMapper.readValue(data, Alert.class);
-
-                //Alert[] alerts2 = objectMapper.readValue(new File("src/main/resources/alerts1.json"), Alert.class);
-
-                //ArrayList<Alert> alerts = objectMapper.readValue(new File("src/main/resources/alerts2.json"), new TypeReference<ArrayList<Alert>>(){});
-                alerts.addAll(Arrays.asList(objectMapper.readValue(new File("src/main/resources/alerts1.json"), Alert[].class)));
-                System.out.println("alertstmp : " + alerts);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    public void loadAlerts() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            alerts.addAll(Arrays.asList(objectMapper.readValue(new File(ALERTS_FILE), Alert[].class)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        public void saveAlerts(){
-            try {
-                ObjectMapper mapper = new ObjectMapper();
-
-                //  Ten blok chciałbym żeby działał - ale on nie chce
-
-                /*
-                StringBuilder jsonAlertsArray=new StringBuilder("[");
-                int size=alerts.size();
-                Alert tmpAlert = new Alert();
-                for(int i=0; i<size; i++) {
-                    tmpAlert = alerts.get(i);
-                    System.out.println("tmpAlert : " + tmpAlert);
-                    String tmpStr=mapper.writeValueAsString(alerts.get(0));
-                    System.out.println(tmpStr);
-                    jsonAlertsArray.append(tmpStr);
-                    if(size-i>1) jsonAlertsArray.append(",");
-                }
-                jsonAlertsArray.append("]");
-                mapper.writeValue(new File("src/main/resources/alerts10.json"),jsonAlertsArray);
-                */
-
-                // A jednoczesnie - działa jego uproszczona wersja poniżej :
-
-//                String jsonAlertsArray = "[" + mapper.writeValueAsString(alerts.get(0));
-                //jsonAlertsArray += "," + mapper.writeValueAsString(alerts.get(1));
-                //jsonAlertsArray += "," + mapper.writeValueAsString(alerts.get(2));
-//                jsonAlertsArray += "," + mapper.writeValueAsString(alerts.get(1))+ "]";
-//                System.out.println("jsonAlertsArray : " + jsonAlertsArray);
-                mapper.writeValue(new File("src/main/resources/alerts10.json"), alerts);
-                System.out.println("Alerty zapisane do pliku alerts10.json");
-
-                // Nie rozumiem dlaczego nie można tego wszystkiego zrobić tak jak w linii poniżej:
-                //mapper.writeValue(new File("src/main/resources/alerts10.json"),ArrayList.class);
-
-                // Z tego co czytam - to ta moja ArrayList'a jest chyba tak naprawdę LinkedHashMap'ą
-                // i pewnie dlatego nie działa nawet coś takiego :
-                //for(Alert a:alerts) System.out.println(a.toString());
-
+    }
+    public void saveAlerts(){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File(ALERTS_FILE), alerts);
+            System.out.println("Alerty zapisane do pliku " + ALERTS_FILE);
         } catch (IOException e) { throw new RuntimeException(e);}
-        }
-        public void showAlerts() {
-            System.out.println("alerts : " + alerts);
-        }
+    }
 
-        public int addToAlerts(Alert alert) {
-                alerts.add(alert);
-                return 0;
-        }
-        //check if the Asset's code is supported by the Application
+
+    //  Function getUserAlerts returns ArrayList of Alerts created by user identified by uID
+    public List<Alert> getUserAlerts(String uID) {
+        List<Alert> tmpAlerts = new ArrayList<>();
+        tmpAlerts.addAll(alerts.stream().filter(c -> uID.equals(c.getUserID())).toList());
+        return tmpAlerts;
+    }
+
+    public int addToAlerts(Alert alert) {
+        System.out.println("Dodajemy alert : " + alert.toString());
+        int result = Assets.checkCurrencyCode(alert.getCurrCode());
+        if (result == 0) alerts.add(alert);
+        return result;
+    }
+    //check if the Asset's code is supported by the Application
 
 
     public Alerts() {
     }
 }
-
--- src/main/java/pl/isa/javaers/Main.java
+++ src/main/java/pl/isa/javaers/Main.java
@@ -1,34 +1,34 @@
 package pl.isa.javaers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Alert> tmpAlerts = new ArrayList<>();
         Assets.loadAssets();
-        Alert alert = new Alert();
-        alert.create("user1","USD",4.3f,true);
-        System.out.println(Assets.checkCurrencyCode("USD"));
-        System.out.println(Assets.checkCurrencyCode("SEK"));
-        GeneratorAlertID.generatorAlertID();
-        GeneratorAlertID.showAlertID();
+        //Alert alert = new Alert();
+        //alert.create("user1","USD",4.3f,true);
 
         Alerts alerts = new Alerts();
-        alerts.showAlerts();
+
+        UI.showAllAlerts();
         alerts.loadAlerts();
-        alerts.showAlerts();
-        alerts.addToAlerts(new Alert("user2", "USD", 3.9f, false));
-        alerts.addToAlerts(new Alert("user2", "EUR", 4.2f, false));
-        alerts.addToAlerts(new Alert("user3", "CHF", 4.4f, false));
-        alerts.addToAlerts(new Alert("user3", "SEK", 3.9f, true));
-        alerts.showAlerts();
+
+        UI.showAllAlerts();
+        if(0!=alerts.addToAlerts(new Alert("user1", "BTC", 180000f, true))) System.out.println("Alert nie został dodany");
+        /*
+        alerts.addToAlerts(new Alert("user2", "GBP", 4.4f, false));
+        alerts.addToAlerts(new Alert("user3", "NOK", 3.7f, false));
+        alerts.addToAlerts(new Alert("user4", "USD", 4.3f, true));
+        alerts.addToAlerts(new Alert("user5", "EUR", 4.25f, false));
+        */
+        //UI.showAllAlerts();
         alerts.saveAlerts();
 
-<<<<<<< Updated upstream
-=======
-        tmpAlerts = alerts.getUserAlerts("user3");
-        UI.showAlerts(tmpAlerts, "user3");
-        System.out.println("Alerty użytkownika user3 : " + tmpAlerts);
+        tmpAlerts = alerts.getUserAlerts("user4");
+        UI.showAlerts(tmpAlerts, "user4");
+        System.out.println("Alerty użytkownika user4 : " + tmpAlerts);
 
-        UI.newAlert("user4");
-
->>>>>>> Stashed changes
     }
 }
