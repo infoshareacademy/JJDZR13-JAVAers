654f0d24e939827e3f6ae0504dedeb5fef5f6654
Tomasz Odwald <tomasz.odwald@gmail.com>
2024-03-03 19:54:52 +0100

    Created web module with Spring

---
+++ web/src/main/java/pl/isa/javaers/JAVAers.java
@@ -0,0 +1,13 @@
+package pl.isa.javaers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAVAers {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JAVAers.class, args);
+	}
+
+}
---
+++ web/src/test/java/pl/isa/javaers/JAVAersTests.java
@@ -0,0 +1,13 @@
+package pl.isa.javaers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JAVAersTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
