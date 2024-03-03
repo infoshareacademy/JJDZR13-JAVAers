5528c5dcddc5fa4609d740453463ab3fbd651cd3
Tomasz Odwald <tomasz.odwald@gmail.com>
2024-03-03 19:49:24 +0100

    Moved console app to separate module

--- console/src/main/java/pl/isa/javaers/ReadJSON.java
+++ console/src/main/java/pl/isa/javaers/ReadJSON.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 public class ReadJSON {
     public static void main(String[] args) {
         try {
-            byte[] data = Files.readAllBytes(Paths.get("src/main/resources/KursyNBP/BGN.json"));
+            byte[] data = Files.readAllBytes(Paths.get("console/src/main/resources/KursyNBP/BGN.json"));
             ObjectMapper objectMapper = new ObjectMapper();
             KursNBP kursBGN = objectMapper.readValue(data, KursNBP.class);
             System.out.println(kursBGN.getCurrency());
---
+++ console/src/main/java/pl/isa/javaers/Settings.java
@@ -0,0 +1,5 @@
+package pl.isa.javaers;
+
+public class Settings {
+    public static final String ALERTS_FILE = "console/src/main/resources/alerty.json";
+}
