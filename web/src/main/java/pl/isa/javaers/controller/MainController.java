d769cfbdb69b11a87ff7ed8103a0f8421116529a
Krzysztof Lachota <krzysztof.lachota@gmail.com>
2024-03-24 18:37:13 +0100

    Web template fragments implementation. Alert list template creation

---
+++ web/src/main/java/pl/isa/javaers/controller/MainController.java
@@ -0,0 +1,19 @@
+package pl.isa.javaers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    String welcome(Model model) {
+        model.addAttribute("content","_welcome");
+        return "index";
+    }
+    @GetMapping("/alerts")
+    String listaAlertów(Model model) {
+        model.addAttribute("content", "_alertsList");
+        return "index";
+    }
+}
