5531cdaf56d795c432de19370770d1eea62d42b0
Marek Posturzyński <marekposturzynski@gmail.com>
2024-04-14 18:54:14 +0200

    fixed rateHistoryForm.html

--- web/src/main/java/pl/isa/javaers/controller/MainController.java
+++ web/src/main/java/pl/isa/javaers/controller/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.isa.javaers.*;
 import pl.isa.javaers.configuration.Settings;
 import pl.isa.javaers.service.RateService;
@@ -35,6 +37,17 @@ public class MainController {
     model.addAttribute("content", "_rates")
             .addAttribute("tmpRates", tmpRates)
             .addAttribute("currencyName", rateName);
-    return "rateHistory";
+    return "rate-history";
+    }
+    @GetMapping("/rates-form")
+    String ratesForm(Model model) {
+        model.addAttribute("content", "_rates-form")
+                .addAttribute("kursNBP", new KursNBP());
+        return "rateHistoryForm";
+    }
+    @PostMapping(value = "/rates-form")
+        String getRatesParamForm(KursNBP kursNBP, Model model){
+        model.addAttribute("content", "_rates-form");
+        return "redirect:/rates";
     }
 }
