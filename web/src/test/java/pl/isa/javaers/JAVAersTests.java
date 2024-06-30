957f01d951cc480ee81b625759c5e00dfd4166b1
Krzysztof Lachota <krzysztof.lachota@gmail.com>
2024-06-30 20:43:15 +0200

    controller.UI class removed

--- web/src/test/java/pl/isa/javaers/JAVAersTests.java
+++ web/src/test/java/pl/isa/javaers/JAVAersTests.java
@@ -1,13 +1,33 @@
 package pl.isa.javaers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JAVAersTests {
+
+	@Autowired
+	WebApplicationContext context;
+	@Autowired
+	MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
+
+	/*void TestSth(){
+		mockMvc.perform(get())
+	}*/
 
 }
